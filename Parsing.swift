f3cedceecf79b377b514863376847e98ffd383f1
Ahmet Karalar <akaralar@gmail.com>
2023-08-06 05:32:03 +0200

    Add day 6

---
+++ Parsing.swift
@@ -0,0 +1,14 @@
+//
+//  Parsing.swift
+//  AdventOfCode
+//
+//  Created by Ahmet Karalar on 06/08/2023.
+//
+
+import Foundation
+
+extension String {
+    var lines: [String.SubSequence] {
+        self.split(separator: "\n")
+    }
+}
