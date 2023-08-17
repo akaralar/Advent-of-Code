e3afe2da46dcb21bff7814b80ef127456bdd8e10
Ahmet Karalar <akaralar@gmail.com>
2023-08-17 17:05:12 +0200

    Add solution for day 10

---
+++ Day10.swift
@@ -0,0 +1,55 @@
+//
+//  Day10.swift
+//  AdventOfCode
+//
+//  Created by Ahmet Karalar on 08/08/2023.
+//
+
+import Foundation
+
+class Day10: Solving {
+    func solve(input: String, iterations: Int) -> String {
+        var nextInput = input
+        var result: String = ""
+        for _ in (0..<iterations) {
+            result = lookAndSay(nextInput)
+            nextInput = result
+        }
+
+        return result
+    }
+    func lookAndSay(_ input: String) -> String {
+
+        var currentCharCount = 0
+        var prevChar: Character? = nil
+        var result = ""
+        for char in input {
+            guard let prev = prevChar else {
+                currentCharCount += 1
+                prevChar = char
+                continue
+            }
+            if char == prev {
+                currentCharCount += 1
+            } else {
+                result.append("\(String(currentCharCount))\(String(prev))")
+                currentCharCount = 1
+            }
+            prevChar = char
+        }
+
+        result.append("\(String(currentCharCount))\(prevChar!)")
+
+        return result
+    }
+
+    func solvePart1() async -> String {
+
+        return String(solve(input: input, iterations: 40).count)
+    }
+
+    func solvePart2() async -> String {
+        return String(solve(input: input, iterations: 50).count)
+    }
+    var input: String { "3113322113" }
+}
