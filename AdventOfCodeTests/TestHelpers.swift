353c8e3167ce97075177ecf14ae017ae455c1174
Ahmet Karalar <akaralar@gmail.com>
2023-12-01 18:36:07 +0100

    Correct the directory structure

---
+++ AdventOfCodeTests/TestHelpers.swift
@@ -0,0 +1,13 @@
+//
+//  TestHelpers.swift
+//  AdventOfCodeTests
+//
+//  Created by Ahmet Karalar on 02/08/2023.
+//
+
+import Foundation
+
+struct TestCase {
+    let input: String
+    let expected: String
+}
