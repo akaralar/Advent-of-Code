846f1abde95690476f5624e272fdb3d1ba3fe9c1
Ahmet Karalar <akaralar@gmail.com>
2023-12-04 12:03:17 +0100

    Add solution for day 4 for 2023 and add formatting for measured run time

--- AdventOfCode/Parsing.swift
+++ AdventOfCode/Parsing.swift
@@ -33,3 +33,27 @@ func MD5(string: String) -> String {
         String(format: "%02hhx", $0)
     }.joined()
 }
+
+let timeFormatter: NumberFormatter = {
+    let f = NumberFormatter()
+    f.maximumFractionDigits = 3
+    return f
+}()
+
+func format(time: Duration) -> String {
+  var time = Double(time.components.seconds) + (Double(time.components.attoseconds) / 1.0e18)
+  let unit: String
+  if time > 1.0 {
+    unit = "s"
+  } else if time > 0.001 {
+    unit = "ms"
+    time *= 1_000
+  } else if time > 0.000_001 {
+    unit = "µs"
+    time *= 1_000_000
+  } else {
+    unit = "ns"
+    time *= 1_000_000_000
+  }
+  return timeFormatter.string(from: NSNumber(value: time))! + unit
+}
--- AdventOfCode/main.swift
+++ AdventOfCode/main.swift
@@ -13,22 +13,22 @@ let kDay = 01
 
 let inputString = "https://adventofcode.com/2015/day/1/input"
 
-let solution = S2303()
+let solution = S2304()
 
 let clock = ContinuousClock()
 var resultPart1: String = ""
-let result1 = clock.measure {
+let timePart1 = clock.measure {
     resultPart1 = solution.solvePart1()
 }
 print("Part 1: \(resultPart1)")
-print("Time 1: \(result1)")
+print("Time 1: \(format(time: timePart1))")
 
 var resultPart2 = ""
-let result2 = clock.measure {
+let timePart2 = clock.measure {
     resultPart2 = solution.solvePart2()
 }
 print("Part 2: \(resultPart2)")
-print("Time 2: \(result2)")
+print("Time 2: \(format(time: timePart2))")
 //var years: [Year] = []
 //for year in (2015...2022) {
 //    years.append(Year(year: year))
