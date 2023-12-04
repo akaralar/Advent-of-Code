96d951a4eed99645e6e7fb541f398c51027fbc3e
Ahmet Karalar <akaralar@gmail.com>
2023-12-04 14:32:42 +0100

    Change second part to collect items in an array instead of a dictionary

--- AdventOfCode/2023/202304.swift
+++ AdventOfCode/2023/202304.swift
@@ -26,36 +26,25 @@ struct S2304: Solving {
     }
     
     func solvePart2() -> String {
-        return input.lines
-            .reduce(into: Dictionary<Int, Int>()) { earned, line in
+        let lines = input.lines
+        return lines
+            .reduce(into: Array(repeating: 1, count: lines.count)) { earned, line in
                 let numbers = line.split(separator: ":")
                     .flatMap { $0.split(separator: "|") }
                     .map { $0.matches(of: regex).map(\.output.1) }
-                let card = numbers[0][0]
-                earned[card, default: 0] += 1
+                let cardIndex = numbers[0][0] - 1
                 earned = Set(numbers[1])
                     .intersection(Set(numbers[2]))
                     .enumerated()
-                    .map { card + $0.0 + 1 }
+                    .map { cardIndex + 1 + $0.0 }
                     .reduce(into: earned) { earned, next in
-                        earned[next, default: 0] += earned[card]!
+                        earned[next] += earned[cardIndex]
                     }
             }
-            .values
             .reduce(0, +)
             .asString
     }
 
-    var testInput: String {
-        """
-        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-        """
-    }
     var input: String {
         """
         Card   1: 13  5 40 15 21 61 74 55 32 56 | 21 57 74 56  7 84 37 47 75 66 68  8 55 22 53 61 40 13 15 41 32 46 95 65  5
