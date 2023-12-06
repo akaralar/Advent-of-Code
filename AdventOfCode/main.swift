11a900fde9178174b47799839e2f2f4b32aa757c
Ahmet Karalar <akaralar@gmail.com>
2023-12-07 00:10:46 +0100

    Clean up main.swift

--- AdventOfCode/main.swift
+++ AdventOfCode/main.swift
@@ -7,29 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
-let kYear = 2015
-let kDay = 01
-
-let inputString = "https://adventofcode.com/2015/day/1/input"
-
 let solution = S2306()
 
 let clock = ContinuousClock()
 var resultPart1: String = ""
 let timePart1 = clock.measure {
-    resultPart1 = solution.solvePart1()
+    print("Part 1 answer: \(solution.solvePart1())")
 }
-print("Part 1: \(resultPart1)")
-print("Time 1: \(format(time: timePart1))")
 
-var resultPart2 = ""
 let timePart2 = clock.measure {
-    resultPart2 = solution.solvePart2()
+    print("Part 2 answer: \(solution.solvePart2())")
 }
-print("Part 2: \(resultPart2)")
-print("Time 2: \(format(time: timePart2))")
-//var years: [Year] = []
-//for year in (2015...2022) {
-//    years.append(Year(year: year))
-//}
+
+print("Part 1 time: \(format(time: timePart1))")
+print("Part 2 time: \(format(time: timePart2))")
