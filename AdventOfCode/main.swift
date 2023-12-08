5a9b0702fc17e7de9fcf5478ca1c46007a6c6ab2
Ahmet Karalar <akaralar@gmail.com>
2023-12-08 16:54:54 +0100

    test solution for day 8

--- AdventOfCode/main.swift
+++ AdventOfCode/main.swift
@@ -7,12 +7,42 @@
 
 import Foundation
 
-let solution = S2307()
+let testInput1 = """
+RL
 
-let testInput = """
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
 """
+
+let testInput2 = """
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+"""
+
 let input = """
 """
+
+let testInput3 = """
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+"""
+let solution = S2308()
 
 let clock = ContinuousClock()
 let timePart1 = clock.measure {
