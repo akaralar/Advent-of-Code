6825acac71cdd05dec93ad3403fcd20b4fc72e93
Ahmet Karalar <akaralar@gmail.com>
2023-12-11 15:14:39 +0100

    Add solution for day 10 of 2023

---
+++ AdventOfCode/2023/202310.swift
@@ -0,0 +1,156 @@
+// Created by Ahmet Karalar for AdventOfCode in 2023
+// Using Swift 5.0
+
+
+import Foundation
+import Algorithms
+
+struct S2310: Solving {
+    func solvePart1(_ input: String) -> Int {
+        let map = input.lines.map { Array($0) }
+        var entrance = startingPoint(of: map)
+        var nextStep = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            .map { (entrance.x + $0.0, entrance.y + $0.1) }
+            .first { exitCoordinate(ofPipeAt: $0, entrance: entrance, map: map) != nil}!
+
+        var steps = 0
+        repeat {
+            guard let temp = exitCoordinate(ofPipeAt: nextStep, entrance: entrance, map: map)
+            else { fatalError() }
+            entrance = nextStep
+            nextStep = temp
+            steps += 1
+        } while map[nextStep.1][nextStep.0] != "S"
+
+        return (steps / 2) + 1
+    }
+
+    func solvePart2(_ input: String) -> Int {
+        var map = input.lines.map { Array($0) }
+        var entrance = startingPoint(of: map)
+        let (east, west, south, north) = ([0, 1], [0, -1], [1, 0], [-1, 0])
+        let possibleFirstSteps = [east, west, south, north]
+            .filter { exitCoordinate(ofPipeAt: (entrance.x + $0[0], entrance.y + $0[1]), entrance: entrance, map: map) != nil }
+
+        let startingPipe: Character = switch (possibleFirstSteps.first!, possibleFirstSteps.last!) {
+        case (east, west): "-"
+        case (east, south): "F"
+        case (east, north): "7"
+        case (west, south): "L"
+        case (west, north): "J"
+        case (south, north): "|"
+        default: fatalError()
+        }
+        
+        map[entrance.y][entrance.x] = startingPipe
+        var nextStep = (entrance.x + possibleFirstSteps.first![0], entrance.y + possibleFirstSteps.first![1])
+        var loop: [Int: [Int: Character]] = [:]
+        repeat {
+            guard let temp = exitCoordinate(ofPipeAt: nextStep, entrance: entrance, map: map)
+            else { fatalError() }
+            if map[nextStep.1][nextStep.0] != "-" {
+                loop[nextStep.1, default: [:]][nextStep.0] = map[nextStep.1][nextStep.0]
+            }
+            entrance = nextStep
+            nextStep = temp
+        } while loop[nextStep.1]?[nextStep.0] == nil
+
+        var insideCount = 0
+        for y in loop.keys.sorted() {
+            guard let row = loop[y] else { continue }
+            var lastBoundaryIndex: Int? = nil
+            var previousBendPipe: Character? = nil
+            for x in row.keys.sorted() {
+                let char = map[y][x]
+                switch char {
+                case "-": fatalError()
+                case "|":
+                    if let index = lastBoundaryIndex {
+                        let sum = (x - index - 1)
+                        insideCount += sum
+                        lastBoundaryIndex = nil
+                    } else {
+                        lastBoundaryIndex = x
+                    }
+                case "F":
+                    if let index = lastBoundaryIndex {
+                        let sum = (x - index - 1)
+                        insideCount += sum
+                    }
+                    previousBendPipe = "F"
+                case "L":
+                    if let index = lastBoundaryIndex {
+                        let sum = (x - index - 1)
+                        insideCount += sum
+                    }
+                    previousBendPipe = "L"
+                case "7":
+                    if previousBendPipe == "L" {
+                        if lastBoundaryIndex == nil {
+                            lastBoundaryIndex = x
+                        } else {
+                            lastBoundaryIndex = nil
+                        }
+                    } else {
+                        if lastBoundaryIndex != nil {
+                            lastBoundaryIndex = x
+                        }
+                    }
+                case "J":
+                    if previousBendPipe == "F" {
+                        if lastBoundaryIndex == nil {
+                            lastBoundaryIndex = x
+                        } else {
+                            lastBoundaryIndex = nil
+                        }
+                    } else {
+                        if lastBoundaryIndex != nil {
+                            lastBoundaryIndex = x
+                        }
+                    }
+                default: fatalError()
+                }
+            }
+        }
+
+        return insideCount
+    }
+
+    func startingPoint(of map: [[Character]]) -> (x: Int, y: Int) {
+        for (y, row) in map.enumerated() {
+            if let x = row.firstIndex(of: "S") {
+                return (x, y)
+            }
+        }
+        fatalError()
+    }
+
+
+    func exitCoordinate(
+        ofPipeAt coordinate: (x: Int, y: Int),
+        entrance: (x: Int, y: Int),
+        map: [[Character]]
+    ) -> (x: Int, y: Int)? {
+        if coordinate.x < 0 || coordinate.y < 0 || coordinate.y >= map.count || coordinate.x >= map[coordinate.y].count { return nil }
+        let pipe = map[coordinate.y][coordinate.x]
+        let (xDiff, yDiff) = (coordinate.x - entrance.x, coordinate.y - entrance.y)
+        switch pipe {
+        case "|" where yDiff != 0: return (coordinate.x, coordinate.y + yDiff)
+        case "-" where xDiff != 0: return (coordinate.x + xDiff, coordinate.y)
+            // entrance 1,1 coordinate 1,2 exit 2,2
+            // entrance 2,2 coordinate 1,2 exit 1,1
+        case "L" where yDiff == 1 || xDiff == -1: return (coordinate.x + yDiff, coordinate.y + xDiff)
+            // entrance 1,1 coordinate 1,2 exit 0,2
+            // entrance 0,2 coordinate 1,2 exit 1,1
+        case "J" where yDiff == 1 || xDiff == 1: return (coordinate.x - yDiff, coordinate.y - xDiff)
+            // entrance 1,2 coordinate 1,1 exit 0,1
+            // entrance 0,1 coordinate 1,1 exit 1,2
+        case "7" where yDiff == -1 || xDiff == 1: return (coordinate.x + yDiff, coordinate.y + xDiff)
+            // entrance 1,2 coordinate 1,1 exit 2,1
+            // entrance 2,1 coordinate 1,1 exit 1,2
+        case "F" where yDiff == -1 || xDiff == -1: return (coordinate.x - yDiff, coordinate.y - xDiff)
+        default:
+            return nil
+        }
+    }
+}
--- AdventOfCode/main.swift
+++ AdventOfCode/main.swift
@@ -11,12 +11,72 @@ let input = """
 """
 
 let testInput1 = """
-0 3 6 9 12 15
-1 3 6 10 15 21
-10 13 16 21 30 45
+-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF
 """
 
-let solution = S2309()
+let testInput2 = """
+7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ
+"""
+
+let testInput3 = """
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+"""
+
+let testInput4 = """
+..........
+.S------7.
+.|F----7|.
+.||OOOO||.
+.||OOOO||.
+.|L-7F-J|.
+.|II||II|.
+.L--JL--J.
+..........
+"""
+
+let testInput5 = """
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+"""
+
+let testInput6 = """
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+"""
+
+let solution = S2310()
 
 let clock = ContinuousClock()
 let timePart1 = clock.measure {
