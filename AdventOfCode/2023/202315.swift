1c5b61e6c337bc2f65da7bd822ddf366fb23133c
Ahmet Karalar <akaralar@gmail.com>
2023-12-15 12:17:26 +0100

    [2023-15] Add solution for day 15

---
+++ AdventOfCode/2023/202315.swift
@@ -0,0 +1,35 @@
+// Created by Ahmet Karalar for AdventOfCode in 2023
+// Using Swift 5.0
+
+
+import Foundation
+import OrderedCollections
+
+class S2315: Solving {
+    func hash(_ str: any StringProtocol) -> Int {
+        str.reduce(0) { (($0 + Int($1.asciiValue!)) * 17) % 256 }
+    }
+
+    func solvePart1(_ input: String) -> Int {
+        input.split(separator: ",").map(hash(_:)).reduce(0, +)
+    }
+
+    func solvePart2(_ input: String) -> Int {
+        return input.split(separator: ",")
+            .map { (prefix: $0.prefix(while: { $0 != "=" && $0 != "-" }), last: $0.last!) }
+            .reduce(into: [Int: OrderedDictionary<Substring, Int>]()) { boxes, next in
+                let box = hash(next.prefix)
+                boxes[box, default: [:]][next.prefix] = Int(String(next.last))
+            }
+            .reduce(0) { sum, next in
+                let (box, lenses) = next
+                let boxSum = lenses.enumerated()
+                    .map { ($0+1, $1) }
+                    .reduce(0) { boxSum, next in
+                        let (slot, (_, focalLength)) = next
+                        return boxSum + (slot * focalLength)
+                    }
+                return sum + ((box + 1) * boxSum)
+            }
+    }
+}
--- AdventOfCode/main.swift
+++ AdventOfCode/main.swift
@@ -11,16 +11,7 @@ let input = """
 """
 
 let testInput1 = """
-O....#....
-O.OO#....#
-.....##...
-OO.#O....O
-.O.....O#.
-O.#..O.#.#
-..O..#O..O
-.......O..
-#....###..
-#OO..#....
+rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
 """
 
 let solution = S2314()
