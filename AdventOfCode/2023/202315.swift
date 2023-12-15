8c0d8f29b445afebb1333a919fa5cfbb552077e6
Ahmet Karalar <akaralar@gmail.com>
2023-12-15 13:06:37 +0100

    [2023-15] Reduce the amount of code

--- AdventOfCode/2023/202315.swift
+++ AdventOfCode/2023/202315.swift
@@ -18,16 +18,8 @@ class S2315: Solving {
         return input.split(separator: ",")
             .map { (label: $0.prefix(while: { $0 != "=" && $0 != "-" }), last: $0.last!) }
             .reduce(into: [Int: OrderedDictionary<Substring, Int>]()) { boxes, next in
-                let box = hash(next.label)
-                boxes[box, default: [:]][next.label] = Int(String(next.last))
+                boxes[hash(next.label), default: [:]][next.label] = Int(String(next.last))
             }
-            .reduce(0) { sum, next in
-                let (box, lenses) = next
-                let boxSum = zip(1..., lenses).reduce(0) { boxSum, next in
-                    let (slot, (_, focalLength)) = next
-                    return boxSum + (slot * focalLength)
-                }
-                return sum + ((box + 1) * boxSum)
-            }
+            .reduce(0) { $0 + (($1.0 + 1) * zip(1..., $1.1.values).reduce(0) { $0 + ($1.0 * $1.1)})}
     }
 }
